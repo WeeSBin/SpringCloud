64cc28cf793952e01578df426929159de6e166e7
sgh15487 <tktma12>
2020-01-08 10:46:07 +0900

    config client

---
+++ ConfigClient/src/main/java/wesbin/configclient/ConfigClientController.java
@@ -0,0 +1,24 @@
+package wesbin.configclient;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ConfigClientController {
+
+    private final ApplicationConfiguration applicationConfiguration;
+
+    public ConfigClientController(ApplicationConfiguration applicationConfiguration) {
+        this.applicationConfiguration = applicationConfiguration;
+    }
+
+    public Map<String, String> welcome(){
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("message", applicationConfiguration.getMessage());
+
+        return map;
+    }
+}
