1d6d645224ac581a59ab9156bfb5099c591dbaf7
sgh15487 <tktma12>
2020-01-14 13:09:02 +0900

    kafkaStream

---
+++ SpringCloud-Stream/WithKafkaStream/src/test/java/wesbin/withkafkastream/WithkafkastreamApplicationTests.java
@@ -0,0 +1,13 @@
+package wesbin.withkafkastream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithkafkastreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
